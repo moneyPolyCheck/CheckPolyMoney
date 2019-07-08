e8343401a0f72bf1e469c471588557b4cccf53ee
anastasiaplyako <anastasiareiz@gmail.com>
2019-07-08 11:33:22 +0700

    updated website layout

--- src/php/main.php
+++ src/php/main.php
@@ -10,13 +10,15 @@ $link = mysqli_connect($host, $user, $pass, $db_name); // Соединяемся
 if (!$link) {
     echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
     exit;
-} else {
-    echo 'ура';
 }
 
 if (isset($_POST["income"])) {
     //Вставляем данные, подставляя их в запрос
-    $sql = mysqli_query($link, "INSERT INTO `income_table` (`income`, `products`, `household_needs`, `rent`, `clothes`, `health`, `sport`, `entertainment`, `car_expenses`,`accumulation`, `other_expenses` ) VALUES ('{$_POST['income']}', '{$_POST['products']}', '{$_POST['household_needs']}','{$_POST['rent']}', '{$_POST['clothes']}', '{$_POST['health']}', '{$_POST['sport']}', '{$_POST['entertainment']}', '{$_POST['car_expenses']}', '{$_POST['accumulation']}', '{$_POST['other_expenses']}' )");
+    $sql = mysqli_query($link, "INSERT INTO `income_table` (`income`, `products`, `household_needs`, `rent`, 
+`clothes`, `health`, `sport`, `entertainment`, `car_expenses`,`accumulation`, `other_expenses` ) 
+VALUES ('{$_POST['income']}', '{$_POST['products']}', '{$_POST['household_needs']}','{$_POST['rent']}', 
+'{$_POST['clothes']}', '{$_POST['health']}', '{$_POST['sport']}', '{$_POST['entertainment']}', 
+'{$_POST['car_expenses']}', '{$_POST['accumulation']}', '{$_POST['other_expenses']}' )");
     //Если вставка прошла успешно
     if ($sql) {
         echo '<p>Данные успешно добавлены в таблицу.</p>';
