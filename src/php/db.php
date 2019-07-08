d45b8b12e4c029595e214842379f65950d5e7d81
LiaMed12 <lia_med@mail.ru>
2019-07-08 18:34:02 +0300

    добавлена активация аккаунта и проработан внешний вид

--- src/php/db.php
+++ src/php/db.php
@@ -1,6 +1,6 @@
 <?php
 
- $connection = mysqli_connect('127.0.0.1','root','','test_db');
+ $connection = mysqli_connect('127.0.0.1','root','','checkpolycash');
  if($connection==false){
 	 echo 'Соединение не работает';
  }
